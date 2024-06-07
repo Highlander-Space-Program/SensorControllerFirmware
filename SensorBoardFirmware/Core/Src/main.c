09cfe3fd45ea9c148cdfaa8e9d2fdc53e285c1cb
ahthae <122731557+ahthae@users.noreply.github.com>
2024-06-07 14:35:56 -0700

    Enable pull-down on LED pin

--- SensorBoardFirmware/Core/Src/main.c
+++ SensorBoardFirmware/Core/Src/main.c
@@ -350,7 +350,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : BUILTIN_LED_Pin */
   GPIO_InitStruct.Pin = BUILTIN_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(BUILTIN_LED_GPIO_Port, &GPIO_InitStruct);
 
